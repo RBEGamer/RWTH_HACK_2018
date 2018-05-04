a75870d091ee66616fec11a321a5c157b92c99aa
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2018-05-04 19:00:27 +0200

    jq fix

---
+++ src/frontend/public/push_notifier.js
@@ -0,0 +1,55 @@
+var socket = null;
+
+function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
+
+  function reps_guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(8)
+        .substring(1);
+    }
+    return s4() + s4()  + s4()  + s4();
+  }
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes;
+    return strTime;
+}            
+
+
+
+
+$(function () {
+    socket = io();
+    socket.emit('event', reps_guid());
+
+    socket.on('message', function (data) {
+       console.log(data);
+
+
+       console.log(data.event_type);
+
+       if(data.event_type == "push"){
+        Push.create(data.payload.header, {
+            body: data.payload.message,
+            icon: 'icon.png',
+            timeout: 4000,
+            onClick: function () {
+                window.focus();
+                this.close();
+            }
+        });
+        
+       }
+      });
+});
