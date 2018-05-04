60681b023b36e016f37cd362793bfc2e266c4848
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2018-05-04 23:31:56 +0200

    wk

--- src/frontend/public/vue_main.js
+++ src/frontend/public/vue_main.js
@@ -1,10 +1,16 @@
+
+  // your code goes here
+
+
 var app = new Vue({
   el: '#app',
   data: {
     all_ticket_count: 1,
-    items: [
-      { TICKET_ID: '0',TICKET_TITLE: 'title1', DEPARTMENT: 'IT', DATE: '01-04-2018', CLIENT: 'AAA', LAST_ACTION: 'answered', STATUS: 'OPEN'},
-      { TICKET_ID: '1',TICKET_TITLE: 'title0', DEPARTMENT: 'IT', DATE: '04-01-2018', CLIENT: 'BBB', LAST_ACTION: 'not answered', STATUS: 'CLOSED'}
+    solved_ticket_count:2,
+    pending_ticket_count:3,
+    recent_ticket_tems: [
+      { id: '0',title: 'title1', created_by: 'IT', created_at: '01-04-2018', last_update: 'AAA', state: 'answered', tag: 'OPEN'},
+    
     ],
     stuff: [
       { STUFF_ID: 'Stuff0',TICKET_ID: 'ticket#0', STATUS: 'OPEN'},
@@ -12,3 +18,13 @@ var app = new Vue({
     ]
   }
 })
+
+
+
+$.getJSON( "http://127.0.0.1:5000/api/tickets/list", function( data ) {
+  if(data == null || data == undefined){console.log("data empty");return;}
+  app.recent_ticket_tems = data
+ 
+  alert( "Load was performed." );
+});
+
