d8c00c679dd642b606896dba6ed73f4fec084a0a
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2018-05-05 00:18:07 +0200

    img fix

--- src/frontend/public/vue_main.js
+++ src/frontend/public/vue_main.js
@@ -1,6 +1,28 @@
 
   // your code goes here
 
+function update_recent_table(){
+  $.getJSON( "http://127.0.0.1:5000/api/tickets/list", function( data ) {
+    if(data == null || data == undefined){console.log("data empty");return;}
+    app.recent_ticket_tems = data
+  var sv = 0;
+  
+    for (i = 0; i < data.length; i++) { 
+    if(data[i].state != undefined && data[i].state == "Done"){
+      sv++;
+      console.log(data.length);
+    }
+    
+  }
+    
+    app.all_ticket_count = data.length;
+    app.solved_ticket_count = sv;
+    app.pending_ticket_count = data.length - sv;
+});
+}
+
+
+
 
 var app = new Vue({
   el: '#app',
@@ -17,14 +39,17 @@ var app = new Vue({
       { STUFF_ID: 'Stuff1',TICKET_ID: 'ticket#1', STATUS: 'CLOSED'}
     ]
   }
-})
-
-
-
-$.getJSON( "http://127.0.0.1:5000/api/tickets/list", function( data ) {
-  if(data == null || data == undefined){console.log("data empty");return;}
-  app.recent_ticket_tems = data
- 
-  alert( "Load was performed." );
 });
+
+
+
+update_recent_table();
+
+setInterval(function(){ update_recent_table(); }, 500);
+
+
+
+
+  //alert( "Load was performed." );
+
 
